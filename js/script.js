b08420bf732807c51a02bc80d55a000e39ee0a16
larissa <cmdrlias@gmail.com>
2019-07-16 18:12:13 -0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,31 +1,43 @@
 $(document).ready(function() {
-    back = $('.backend');
-    front = $('.frontend');
+    var back = $('.backend');
+    var front = $('.frontend');
 
     back.css('z-index', '0');
     front.css('z-index', '0');
 
+    var zIndexBack = back.css('z-index');
+    var zIndexFront = front.css('z-index');
+
+    // zIndexBack <= zIndexFront ? zIndexBack = zIndexFront + 1 : zIndexBack
+
     $('.left-half').hover(function() {
         back.css('left', '0');
         front.css('left', '100%');
-        back.css('z-index', '99900');
+        back.css('z-index', zIndexBack <= zIndexFront ? zIndexBack = zIndexFront + 1 : zIndexBack);
         $('.backend .desc').css('opacity', '1');
-        $('.frontend .desc').css('opacity', '0');
+        $('.frontend .desc').css('opacity', '0'); 
     }, function() {
         front.css('left', '50%');
         back.css('left', '-50%');
+        back.css('z-index', '0');
         $('.backend .desc').css('opacity', '0');
+        zIndexBack = 0;
     });
+
+    // zIndexFront <= zIndexBack ? zIndexFront = zIndexBack + 1 : zIndexFront
     
     $('.right-half').hover(function() {
         front.css('left', '0');
         back.css('left', '-100%');
-        front.css('z-index', '9999900');
+        front.css('z-index', zIndexFront <= zIndexBack ? zIndexFront = zIndexBack + 1 : zIndexFront);
         $('.frontend .desc').css('opacity', '1');
         $('.backend .desc').css('opacity', '0');
+        $('.arrow-down').css('opacity', '0');
     }, function() {
         front.css('left', '50%');
         back.css('left', '-50%');
+        back.css('z-index', '0');
         $('.frontend .desc').css('opacity', '0');
+        zIndexFront = 0;
     });    
 });
